7e95c943533e77a07f864fd9bbdc81fbd57067d2
purrleterian <jcno307@gmail.com>
2023-11-24 22:15:01 -0300

    frame assembler working

--- src/main.h
+++ src/main.h
@@ -1,5 +1,6 @@
-#define WIDTH 35
-#define HEIGHT 10
+#define WIDTH 65
+#define HEIGHT 20
+#define populationSize WIDTH* HEIGHT
 
 // I could just make an array of 0 and 1, but a struct
 // can make the code more scalable
@@ -10,6 +11,8 @@ typedef struct Cell {
 void setCellState(int (*population)[WIDTH], int x, int y, int state);
 int getSurrounding(int population[HEIGHT][WIDTH], int x, int y);
 void updatePopulation(int (*population)[WIDTH]);
+
+void assembleFrame(int population[HEIGHT][WIDTH]);
 
 void initializePopulation(int (*ptrPopulation)[WIDTH]);
 void displayPopulation(int population[HEIGHT][WIDTH]);
