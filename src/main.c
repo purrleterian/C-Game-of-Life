1e34008f256009303baaa254d0ec833bb7d37955
purrleterian <jcno307@gmail.com>
2024-08-23 11:43:53 -0300

    fixed display error

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
         assembleFrame(population);
         updatePopulation(population);
         Sleep(10);
-        system("cls");
+        system("clear");
     }
 
     return 0;
